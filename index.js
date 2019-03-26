759d0106655d6113171780ffce70524b2f6844a7
pankajladhar <ladharpankaj@gmail.com>
2019-03-26 18:51:51 +0530

    sdsd

--- index.js
+++ index.js
@@ -5,39 +5,39 @@ var path = require('path');
 var crypto = require('crypto');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 const key = "yGf2xak9";
 const salt = "Vg1fM0UYz9";
 const txnid = `GR-${new Date().getTime()}`;
 const product = {
     "p-1-1": {
-        "amount": "1000",
-        "label": "Alphanso 1 dozon",
+        "amount": "599",
+        "label": "Alphanso 1/2 dozon",
     },
     "p-1-2": {
-        "amount": "2000",
-        "label": "Alphanso 2 dozon",
+        "amount": "899",
+        "label": "Alphanso 1 dozon",
     },
     "p-1-3": {
-        "amount": "3000",
-        "label": "Alphanso 3 dozon",
+        "amount": "1499",
+        "label": "Alphanso 2 dozon",
     },
     "p-2-1": {
-        "amount": "1000",
-        "label": "Banganapalli 1 dozon",
+        "amount": "599",
+        "label": "Banganapalli 1/2 dozon",
     },
     "p-2-2": {
-        "amount": "2000",
-        "label": "Banganapalli 2 dozon",
+        "amount": "899",
+        "label": "Banganapalli 1 dozon",
     },
     "p-2-3": {
-        "amount": "3000",
-        "label": "Banganapalli 3 dozon",
+        "amount": "1499",
+        "label": "Banganapalli 2 dozon",
     }
 }
 
-const hiddenForm = (amount,name, email, phone, hash, prodInfo) => {
+const hiddenForm = (amount, name, email, phone, hash, prodInfo) => {
     return '<form id=\"myForm\" action=\"https://sandboxsecure.payu.in/_payment" method=\"post\">' +
         '<input type=\"hidden\" name=\"key\" value=\"' + key + '\" />' +
         '<input type=\"hidden\" name=\"txnid\" value=\"' + txnid + '\" />' +
@@ -46,8 +46,8 @@ const hiddenForm = (amount,name, email, phone, hash, prodInfo) => {
         '<input type=\"hidden\" name=\"firstname\" value=\"' + name + '\" />' +
         '<input type=\"hidden\" name=\"email\" value=\"' + email + '\" />' +
         '<input type=\"hidden\" name=\"phone\" value=\"' + phone + '\" />' +
-        '<input type=\"hidden\" name=\"surl\" value=\"http://localhost:4000/confirmation\" />' +
-        '<input type=\"hidden\" name=\"furl\" value=\"http://localhost:4000/error\" />' +
+        '<input type=\"hidden\" name=\"surl\" value=\"https://ancient-gorge-65326.herokuapp.com/confirmation\" />' +
+        '<input type=\"hidden\" name=\"furl\" value=\"https://ancient-gorge-65326.herokuapp.com/error\" />' +
         '<input type=\"hidden\" name=\"hash\" value=\"' + hash + '\" />' +
         '<input type=\"hidden\" name=\"service_provider\" value=\"payu_paisa\" />' +
         '</form>';
@@ -61,14 +61,14 @@ app.post('/', (req, res) => {
     });
 
     req.on('end', function () {
-        let {fname, email, phone, pinfo} = JSON.parse(strdat);
-        
-        const prodInfo = pinfo.map((p)=> {
+        let { fname, email, phone, pinfo } = JSON.parse(strdat);
+
+        const prodInfo = pinfo.map((p) => {
             return product[p]['label']
         }).join(' and ')
-           
+
         let amount = parseInt(product[pinfo[0]]['amount']) + parseInt(pinfo[1] ? product[pinfo[1]]['amount'] : 0)
-        
+
         let cryp = crypto.createHash('sha512');
         let text = key + '|' + txnid + '|' + amount + '|' + prodInfo + '|' + fname + '|' + email + '|||||||||||' + salt;
         cryp.update(text);
@@ -79,7 +79,7 @@ app.post('/', (req, res) => {
         res.end(form);
     });
 })
-app.get('*', (req, res) => {  
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
@@ -87,8 +87,8 @@ app.post('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
-// app.listen(4000, () => {
-//     console.log("Server started .... ")
-// });
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
-module.exports = app
+// module.exports = app
